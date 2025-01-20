82aeeacbe17cf8c914e851962a3a90aee31f26be
vedasjad <96325125+vedasjad@users.noreply.github.com>
2025-01-20 21:09:08 +0530

    App Icon added

--- lib/features/auth/services/auth_services.dart
+++ lib/features/auth/services/auth_services.dart
@@ -2,8 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 Future<UserCredential> signInWithGoogle() async {
-  final GoogleSignInAccount? googleUser =
-      await GoogleSignIn(hostedDomain: 'akgec.ac.in').signIn();
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
   final GoogleSignInAuthentication? googleAuth =
       await googleUser?.authentication;
